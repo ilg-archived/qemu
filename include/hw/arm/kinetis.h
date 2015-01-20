c9812a408d6959dccc2a1443d34dff52fd3bdc73
Liviu Ionescu <ilg@livius.net>
2015-01-20 23:35:13 +0200

    kinetis header duplicate definition removed

--- include/hw/arm/kinetis.h
+++ include/hw/arm/kinetis.h
@@ -16,7 +16,6 @@ void kinetis_board_init(MachineState *machine, QEMUMachine *qm);
 
 qemu_irq *mk20dx128vlh5_mcu_init(MachineState *machine);
 qemu_irq *mk64fn1m0vll12_mcu_init(MachineState *machine);
-qemu_irq *mkl25z128vlk4_mcu_init(MachineState *machine);
 qemu_irq *mk60fn1m0vlq12_mcu_init(MachineState *machine);
 qemu_irq *mkl25z128vlk4_mcu_init(MachineState *machine);
 qemu_irq *mkl26z128vlh4_mcu_init(MachineState *machine);
@@ -25,3 +24,4 @@ qemu_irq *mk22fn512vlh12_mcu_init(MachineState *machine);
 qemu_irq *mkl43z256vlh4_mcu_init(MachineState *machine);
 
 #endif /* HW_ARM_KINETIS_H */
+
