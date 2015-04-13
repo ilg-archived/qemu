7be5f4118adbf9b41a04523cc4e14a557af9f7a5
Liviu Ionescu <ilg@livius.net>
2015-04-13 23:40:44 +0300

    arm semi: stderr goes to stderr.

--- target-arm/arm-semi.c
+++ target-arm/arm-semi.c
@@ -222,7 +222,12 @@ uint32_t do_arm_semihosting(CPUARMState *env)
             return (uint32_t)-1;
         }
         if (strcmp(s, ":tt") == 0) {
+#if defined(CONFIG_GNU_ARM_ECLIPSE)
+            /* Mode is 0="r" for stdin, 4="w" for stdout, 8="a" for stderr */
+            int result_fileno = arg1 < 4 ? STDIN_FILENO : (arg1 < 8 ? STDOUT_FILENO : STDERR_FILENO);
+#else
             int result_fileno = arg1 < 4 ? STDIN_FILENO : STDOUT_FILENO;
+#endif /* defined(CONFIG_GNU_ARM_ECLIPSE) */
             unlock_user(s, arg0, 0);
             return result_fileno;
         }
