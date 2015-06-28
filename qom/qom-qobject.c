7ac4812305ffb14ecba377fc2f83c56de43f901d
Liviu Ionescu <ilg@livius.net>
2015-06-28 15:05:51 +0300

    qom-qopbjects.c: log_mask for setter
    - remove apostrophs

--- qom/qom-qobject.c
+++ qom/qom-qobject.c
@@ -47,7 +47,7 @@ void object_property_set_qobject(Object *obj, QObject *value, const char *name,
 #if defined(CONFIG_GNU_ARM_ECLIPSE)
     if (qemu_loglevel & LOG_TRACE) {
         char buf[100];
-        qemu_log_mask(LOG_TRACE, "%s('%s', '%s', '%s')\n", __FUNCTION__,
+        qemu_log_mask(LOG_TRACE, "%s(%s, %s, '%s')\n", __FUNCTION__,
                 object_get_typename(obj), name,
                 dump_value(value, buf, sizeof(buf)));
     }
